ac76e102d7e8bf9a7fb7f8e0e7c64f9cb96247ae
su apt install kuma <maxxozou@gmail.com>
2024-09-27 13:50:02 +0200

    Version 0.1

---
+++ src/main/java/net/kuma/ascendedalloys/AscendedAlloysClient.java
@@ -0,0 +1,10 @@
+package net.kuma.ascendedalloys;
+
+import net.fabricmc.api.ClientModInitializer;
+
+public class AscendedAlloysClient implements ClientModInitializer {
+    @Override
+    public void onInitializeClient() {
+
+    }
+}
---
+++ src/main/java/net/kuma/ascendedalloys/AscendedAlloysDataGenerator.java
@@ -0,0 +1,11 @@
+package net.kuma.ascendedalloys;
+
+import net.fabricmc.fabric.api.datagen.v1.DataGeneratorEntrypoint;
+import net.fabricmc.fabric.api.datagen.v1.FabricDataGenerator;
+
+public class AscendedAlloysDataGenerator implements DataGeneratorEntrypoint {
+	@Override
+	public void onInitializeDataGenerator(FabricDataGenerator fabricDataGenerator) {
+
+	}
+}
